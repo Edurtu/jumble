15bdbf716b7bee465e1ffb6fe98df8a7f6d4a3b6
Edurtu <edurtu@gmail.com>
2015-09-05 10:45:33 +0200

    Imprime números - Python
    Programa que imprime números por pantalla.
    Author: Eduardo Ortega

---
+++ print_nums.py
@@ -0,0 +1,29 @@
+﻿"""
+	Programa que imprime números por pantalla.
+	Author: Eduardo Ortega
+"""
+
+# ---------------------------------
+# Función principal
+# ---------------------------------
+def main():
+	
+	# presentación del programa
+	print "************************************" + "\n"
+	print u"Imprimir por pantalla todos los números entre 10 y 20:" + "\n"
+	
+	# programa y resultado
+	for x in range(10, 20+1):
+		print x
+	
+	# despedida del programa
+	print ""
+	print u"\n¡Gracias y hasta la próxima!\n"
+	print "************************************"
+
+	
+# ---------------------------------
+# Ejecuta el programa
+# ---------------------------------
+
+main()
